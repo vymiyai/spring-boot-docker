e0217694bf3376c76dc6dd57362bfad2a62fad28
vymiyai <vymiyai@gmail.com>
2017-06-18 01:04:52 +0200

    Added list based injection in setter.

--- src/main/java/com/rhcloud/tutorials/spring_boot_docker/BeanComponent.java
+++ src/main/java/com/rhcloud/tutorials/spring_boot_docker/BeanComponent.java
@@ -1,9 +1,11 @@
 package com.rhcloud.tutorials.spring_boot_docker;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.rhcloud.tutorials.spring_boot_docker.beans.BeanComponent1;
+import com.rhcloud.tutorials.spring_boot_docker.beans.IBeanComponent;
 
 @Component
 public class BeanComponent {
@@ -11,7 +13,8 @@ public class BeanComponent {
 	public String text = "";
 
 	@Autowired
-	void setBeanComponent1(BeanComponent1 bc1) {
-		this.text = bc1.execute();
+	void setBeanComponent1(List<IBeanComponent> beans) {
+		for(IBeanComponent bc : beans)
+			this.text = this.text + " " + bc.execute();
 	}
 }
